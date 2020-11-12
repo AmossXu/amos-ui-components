234fe0bcd25ec24e1fcdb81af311abf0180c482b
CartiXu <mots.xu@gmail.com>
2020-11-12 22:24:19 +0800

    add build config

--- src/conponents/Icon/icon.stories.tsx
+++ src/conponents/Icon/icon.stories.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { Meta, Story} from '@storybook/react/types-6-0';
-import Icon, { IconProps } from './icon';
+import { Meta, Story } from "@storybook/react/types-6-0";
+import Icon, { IconProps } from "./icon";
 export default {
-  title: '示例/Icon',
+  title: "示例/Icon",
   component: Icon,
 } as Meta;
 
-const BasicIcon: Story<IconProps> = (args) => <Icon {...args}/>
+const BasicIcon: Story<IconProps> = (args) => <Icon {...args} />;
 
-export const Default = BasicIcon.bind({})
+export const Default = BasicIcon.bind({});
+Default.args = {
+  icon: "check-circle",
+  theme: "primary",
+  size: "10x",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
-export { default as Button } from './conponents/Button'
-export { default as Menu } from './conponents/Menu'
-export { default as Icon } from './conponents/Icon'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+
+export { default as Button } from "./conponents/Button";
+export { default as Menu } from "./conponents/Menu";
+export { default as Icon } from "./conponents/Icon";
